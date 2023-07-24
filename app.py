ea3a6e0ec4a5a4f512b342b90a53bf1ddafaf613
Mayara Sebastiao <mayara.psebastiao@gmail.com>
2023-07-24 13:38:17 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,51 @@
+familia = ["Roger","Cris","Manu","Vini","Selina"]
+#print(familia)
+#print(familia[0])
+#print(familia[-1])
+#print(familia[2:])
+#print(familia[2:4])
+
+#print(familia)
+#familia[3] = "Roger"
+#print(familia)
+
+#familia.extend(["Fernando","Rosania"])
+#print(familia)
+
+#familia.append("Spock")
+#print(familia)
+
+#familia.insert(2,"Spock")
+#print(familia)
+
+#familia.pop()
+#print(familia)
+
+#familia.remove("Spock")
+#print(familia)
+
+#familia.clear()
+#print(familia)
+
+#print(familia.index("Roger"))
+#print(familia.count("Spock"))
+
+#idade_familia=[34,36,13,11,2]
+#print(idade_familia)
+#idade_familia.sort()
+#print(idade_familia)
+#idade_familia.reverse()
+#print(idade_familia)
+
+#familia2 = familia.copy()
+#print(familia2)
+#familia.remove("Roger")
+#print(familia)
+#print(familia2)
+
+coordenadas = (-49,-36)
+print(coordenadas)
+
+
+
+
